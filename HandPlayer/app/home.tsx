61d7a5b96b55cedb0aac003a6f7d8ca2c676f1f5
UptGiant <105814982+UptGiant@users.noreply.github.com>
2024-05-15 20:02:20 -0500

    pages

---
+++ HandPlayer/app/home.tsx
@@ -0,0 +1,11 @@
+import { View, StyleSheet, Text } from 'react-native'
+
+export default function Home() {
+  return (
+    <View>
+      return(
+        <Text>Hello World!</Text>
+      )
+    </View>
+  )
+}
